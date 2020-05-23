ba6a0b7feba82076b15e481df7d6a45180b66cb1
viewv <zxnnet@gmail.com>
2020-05-23 12:19:35 +0800

    emmm I really don't know how to use php

--- get_article_info.php
+++ get_article_info.php
@@ -28,40 +28,40 @@ if (empty($_GET['url'])) {
 };
 
 //下载封面图，转存到alicdn
-if (!empty($result['cover'])) {
-    $ch = curl_init($result['cover']);
-    curl_setopt_array($ch, [
-        CURLOPT_VERBOSE => true,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_SSL_VERIFYHOST => false,
-        CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
-    ]);
-    $filename = md5(mt_rand()) . '.jpg';
-    file_put_contents($filename, curl_exec($ch));
-    curl_close($ch);
+// if (!empty($result['cover'])) {
+//     $ch = curl_init($result['cover']);
+//     curl_setopt_array($ch, [
+//         CURLOPT_VERBOSE => true,
+//         CURLOPT_RETURNTRANSFER => true,
+//         CURLOPT_FOLLOWLOCATION => true,
+//         CURLOPT_SSL_VERIFYPEER => false,
+//         CURLOPT_SSL_VERIFYHOST => false,
+//         CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+//     ]);
+//     $filename = md5(mt_rand()) . '.jpg';
+//     file_put_contents($filename, curl_exec($ch));
+//     curl_close($ch);
 
-    $ch = curl_init('https://api.superbed.cn/upload');
-    curl_setopt_array($ch, [
-        CURLOPT_VERBOSE => true,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_SSL_VERIFYPEER => false,
-        CURLOPT_SSL_VERIFYHOST => false,
-        CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => [
-            'token' => 'fd9379f57917407f8239dd03a3de0248',
-            'sync' => 1,
-            'src' => $result['cover'],
-        ],
-    ]);
-    $result['cover'] = json_decode(curl_exec($ch), true)['url'];
-    curl_close($ch);
+//     $ch = curl_init('https://api.superbed.cn/upload');
+//     curl_setopt_array($ch, [
+//         CURLOPT_VERBOSE => true,
+//         CURLOPT_RETURNTRANSFER => true,
+//         CURLOPT_FOLLOWLOCATION => true,
+//         CURLOPT_SSL_VERIFYPEER => false,
+//         CURLOPT_SSL_VERIFYHOST => false,
+//         CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+//         CURLOPT_POST => true,
+//         CURLOPT_POSTFIELDS => [
+//             'token' => 'fd9379f57917407f8239dd03a3de0248',
+//             'sync' => 1,
+//             'src' => $result['cover'],
+//         ],
+//     ]);
+//     $result['cover'] = json_decode(curl_exec($ch), true)['url'];
+//     curl_close($ch);
 
-    unlink($filename);
-}
+//     unlink($filename);
+// }
 
 $result['success'] = !empty($result['title']) && !empty($result['cover']);
 echo json_encode($result, JSON_UNESCAPED_UNICODE);
